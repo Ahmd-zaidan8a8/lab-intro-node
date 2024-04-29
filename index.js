049e6c569487f6ce42ec143fcfcdbc84cbb38285
Ahmad Zaidan <ahmad.zaidan8a8@gmail.com>
2024-04-29 20:06:16 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    if (this.length === 0) {
+      this.items.push(item);
+    } else {
+      let index = 0;
+      while (index < this.length && item > this.items[index]) {
+        index++;
+      }
+      this.items.splice(index, 0, item);
+    }
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <= 0 || pos > this.length) throw new Error("OutOfBounds");
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) throw new Error("EmptySortedList");
 
-  min() {}
+    let max = this.items[0];
+    this.items.forEach((item) => {
+      max = max > item ? max : item;
+    });
 
-  sum() {}
+    return max;
+  }
 
-  avg() {}
+  min() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+
+    let min = this.items[0];
+    this.items.forEach((item) => {
+      min = min < item ? min : item;
+    });
+
+    return min;
+  }
+
+  sum() {
+    return this.items.reduce((acc, current) => acc + current, 0);
+  }
+
+  avg() {
+    if (this.length === 0) throw new Error("EmptySortedList");
+    return this.sum() / this.items.length;
+  }
 }
 
 module.exports = SortedList;
